35b245b6ca6dee2dfae4513455edeb5f599cbcff
web dev <154662817+D3XH3R@users.noreply.github.com>
2024-01-26 16:26:56 +0300

    Bootstrap project

---
+++ js/app.js
@@ -0,0 +1,26 @@
+let links = document.querySelectorAll(`.nav-link`);
+let bars = document.querySelector(`.navbar-toggler`);
+let show = document.querySelector(`.navbar-collapse`);
+let li = document.querySelectorAll(`.projects .container ul li`);
+
+links.forEach((i) => {
+  i.addEventListener(`click`, () => {
+    bars.setAttribute(`aria-expanded`, `false`);
+    show.classList.remove(`show`);
+    links.forEach(function (e) {
+      e.classList.remove(`active`);
+    });
+    i.classList.add(`active`);
+  });
+});
+
+li.forEach((i) => {
+  i.addEventListener(`click`, () => {
+    li.forEach((e) => {
+      e.classList.remove(`active`);
+      e.classList.remove(`rounded-pill`);
+    });
+    i.classList.add(`active`);
+    i.classList.add(`rounded-pill`);
+  });
+});
